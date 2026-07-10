f7d5f1a186fa6da162475b4c400496c195b0efce
AnimeFan21-86 <hhabib@turing.ac.uk>
2026-07-10 16:29:07 +0100

    found at the avg mass of penguin by sex

--- penguin.py
+++ penguin.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+import matplotlib as plt
+
+from palmerpenguins import load_penguins
+df = load_penguins()
+print(df.head())
+mass_avg = df.groupby("sex")["body_mass_g"].mean()  #filters out sex and then corresponding mass
+print(mass_avg)
+
+
