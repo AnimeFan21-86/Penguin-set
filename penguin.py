1a9f3b885bd90613c87afce3ce71ac760bdc2b46
AnimeFan21-86 <hhabib@turing.ac.uk>
2026-07-10 16:56:05 +0100

    included a juypter file to upload graphs, filtered out weights for female and male penguins

--- penguin.py
+++ penguin.py
@@ -5,7 +5,10 @@ import matplotlib as plt
 from palmerpenguins import load_penguins
 df = load_penguins()
 print(df.head())
-mass_avg = df.groupby("sex")["body_mass_g"].mean()  #filters out sex and then corresponding mass
+mass_avg = df.groupby("sex")["body_mass_g"].mean() #filters out sex and then corresponding mass
 print(mass_avg)
+mass_avg.plot(kind='bar')
+
+
 
 
